1b5fee433eba08a5382fea2fc91f51e2088f756a
KhusDM <d.xusnetd@yandex.ru>
2022-06-23 20:44:08 +0300

    react-quiz

---
+++ src/components/ActiveQuiz/AnswerList/AnswerList.js
@@ -0,0 +1,23 @@
+﻿import React from "react";
+import classes from './AnswerList.module.css'
+import AnswerItem from "./AnswerItem/AnswerItem";
+
+const AnswerList = (props) => {
+    return (
+        <ul className={classes['AnswerList']}>
+            {
+                props.answers.map((answer, index) => {
+                    return (
+                        <AnswerItem key={index}
+                                    state={props.state ? props.state[answer.id] : null}
+                                    answer={answer}
+                                    onAnswerClick={props.onAnswerClick}
+                        />
+                    );
+                })
+            }
+        </ul>
+    )
+}
+
+export default AnswerList;
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,83 @@
+﻿import React, {Component} from "react";
+import classes from "./Quiz.module.css";
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
+
+class Quiz extends Component {
+    state = {
+        activeQuestion: 0,
+        answerState: null,
+        quiz: [
+            {
+                id: 1,
+                question: 'Какого цвета небо?',
+                rightAnswerId: 2,
+                answers: [
+                    {text: 'Чёрный', id: 1},
+                    {text: 'Синий', id: 2},
+                    {text: 'Красный', id: 3},
+                    {text: 'Зелённый', id: 4},
+                ]
+            },
+            {
+                id: 2,
+                question: 'Какого цвета небо?',
+                rightAnswerId: 2,
+                answers: [
+                    {text: 'Чёрный', id: 1},
+                    {text: 'Синий', id: 2},
+                    {text: 'Красный', id: 3},
+                    {text: 'Зелённый', id: 4},
+                ]
+            }
+        ]
+    };
+
+    onAnswerClickHandler = (answerId) => {
+        const question = this.state.quiz[this.state.activeQuestion];
+        if (question.rightAnswerId === answerId) {
+            this.setState({
+                answerState: {[answerId]: 'success'}
+            });
+
+            const timeout = setTimeout(() => {
+                if (this.isQuizFinished()) {
+                    console.log('Finished');
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    });
+                }
+
+                clearTimeout(timeout);
+            }, 1000);
+        } else {
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            });
+        }
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length;
+    }
+
+    render() {
+        return (
+            <div className={classes['Quiz']}>
+                <div className={classes['QuizWrapper']}>
+                    <h1>Ответьте на все вопросы</h1>
+                    <ActiveQuiz question={this.state.quiz[this.state.activeQuestion].question}
+                                answers={this.state.quiz[this.state.activeQuestion].answers}
+                                onAnswerClick={this.onAnswerClickHandler}
+                                quizLenght={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion + 1}
+                                state={this.state.answerState}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Quiz
